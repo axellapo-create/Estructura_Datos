9dd1a802194c51cf6a67cb7bafc4fe250b178896
unknown <axellapo33@gmail.com>
2026-06-12 13:07:38 -0500

    Feat: Learning Javascript for noobscis

---
+++ Unidad02/00Javascript/tipos-datos.js
@@ -0,0 +1,36 @@
+//PRIMITIVOS
+let nombre = 'Axel'
+
+//NUMERICO
+let edad = 18
+
+//BOOLEANOS
+let esMayordeEdad = true
+
+//null
+let noHayValor = null
+
+//undefined
+let noDefinido = undefined
+
+//symbol  valor unico
+let simboloUnico = Sumbol('único')
+
+//datos gigantes bigint 
+let numeroGrande = 2n
+
+//COMPLETOS
+//object
+let carro ={
+    marca : 'Tesla',
+    modelo : 'Model S'
+}
+
+//arrays
+let frutas = ['manzana' , 'pera', 'piña']
+
+//funcoines
+function saludar(){}
+
+
+
