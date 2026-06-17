0f46b0451ac76700f132beaf9605dae8b9f36489
unknown <axellapo33@gmail.com>
2026-06-17 10:19:58 -0500

    Feat: mejora del HTML y operadores de comparacion

--- Unidad02/00Javascript/operadores-comparacion.js
+++ Unidad02/00Javascript/operadores-comparacion.js
@@ -1,19 +1,37 @@
-/*
-==
-===
-!=
-!==
+// ==
+console.log(5 == "5"); // true
 
-<
->
+// ===
+console.log(5 === "5"); // false
 
-AND
- &&
+// !=
+console.log(5 != "6"); // true
 
- OR
- ||
+// !==
+console.log(5 !== "5"); // true
 
- NOT
- !
- 
-*/
+// <
+console.log(3 < 5); // true
+
+// >
+console.log(10 > 7); // true
+
+// &&
+let edad = 20;
+let tieneEntrada = true;
+
+console.log(edad >= 18 && tieneEntrada);
+// true
+
+// ||
+let esAdmin = false;
+let esProfesor = true;
+
+console.log(esAdmin || esProfesor);
+// true
+
+// !
+let llueve = false;
+
+console.log(!llueve);
+// true
